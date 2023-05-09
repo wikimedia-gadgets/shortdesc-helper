4c3f0c2cf161c62214a19b9ed997e27824a1bb99
Mayavani Iyer <mayavani.iyer@gmail.com>
2023-05-08 22:42:01 -0700

    Update version numbers

--- Shortdesc-helper.js
+++ Shortdesc-helper.js
@@ -8,7 +8,7 @@
  *
  */
 /**
- * Shortdesc helper: v3.5.2
+ * Shortdesc helper: v3.5.3
  * Documentation at en.wikipedia.org/wiki/Wikipedia:Shortdesc_helper
  * The documentation includes instructions for using this gadget on other wikis.
  * Shows short descriptions, and allows importing wikidata descriptions, adding descriptions,
